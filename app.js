7faac8143e4ba0bab4a8d478f8e64461908cca41
Leah Miller <leahmiller1969@gmail.com>
2017-07-26 19:44:57 -0700

    css file not linking with user.mustache

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const mustacheExpress = require('mustache-express')
+const data = require('./data')
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.get("/", function(req, res, next){
+  res.render("index", data)
+})
+////////////////create name////
+app.get("/user/:username", function(req,res,next){
+  var user = data.users.filter(function(person){
+    return person.username === req.params.username ////same name here////
+  })[0]
+
+  res.render("user", user)
+})
+
+
+app.listen(3000, function(){
+  console.log("App running on port 3000")
+})
